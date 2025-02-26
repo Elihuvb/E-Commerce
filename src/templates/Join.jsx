bcde3cc94f53e449f079e63000772250fb82d992
Elihu Valdez <elihuvb@gmail.com>
2025-02-26 08:10:16 -0300

    Nav deleted and Join.jsx created

---
+++ src/templates/Join.jsx
@@ -0,0 +1,13 @@
+import Signup from './Signup'
+import Loggin from './Loggin'
+
+function Join() {
+  return (
+    <div>
+        <Signup />
+        <Loggin />
+    </div>
+  )
+}
+
+export default Join
